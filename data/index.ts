a14849e2dbdb22dc60202a8be35ff520e79c8a7a
razal <razalsha091@.com>
2024-07-11 18:30:20 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,173 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "We prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "We are very flexible with time zone ",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "GROW WITH US",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Driving Digital Success with Innovative Marketing..",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Cutting-edge digital marketing for brand growth.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/mkt.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "CM academy",
+    des: "branding and digial services for an educational institute located in kerala,india.",
+    img: "/p1.svg",
+    iconLists: ["/ic1.jpg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Product Advertisement",
+    des: "product advertisement posters and social media posters for a gadget store .",
+    img: "/b2.jpeg",
+    iconLists: ["/ic1.jpg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "LOGO",
+    des: "Crafting Unique, Memorable Logos That Define Your Brand’s Identity...",
+    img: "/b3.jpeg",
+    iconLists: ["/ic1.jpg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Social Media Posters",
+    des: "Creating Eye-Catching Social Media Posters That Engage and Inspire Audiences...",
+    img: "/b4.jpeg",
+    iconLists: ["/ic1.jpg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Offmarket was an absolute pleasure.their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. offmarket's enthusiasm for every facet of Marketing truly stands out. If you're seeking to elevate your  elevate your brand, offmarket is the ideal partner.",
+    name: "Muhsin Fazfary",
+    title: "Director of CM Academy",
+  },
+  {
+    quote:
+      "Collaborating with Offmarket was an absolute pleasure.their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. offmarket's enthusiasm for every facet of Marketing truly stands out. If you're seeking to elevate your  elevate your brand, offmarket is the ideal partner.",
+    name: "Muhsin Fazfary",
+    title: "Director of CM Academy",
+  },
+  {
+    quote:
+      "Collaborating with Offmarket was an absolute pleasure.their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. offmarket's enthusiasm for every facet of Marketing truly stands out. If you're seeking to elevate your  elevate your brand, offmarket is the ideal partner.",
+    name: "Muhsin Fazfary",
+    title: "Director of CM Academy",
+  },
+  {
+    
+    quote:
+      "Collaborating with Offmarket was an absolute pleasure.their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. offmarket's enthusiasm for every facet of Marketing truly stands out. If you're seeking to elevate your  elevate your brand, offmarket is the ideal partner.",
+    name: "Muhsin Fazfary",
+    title: "Director of CM Academy",
+  },
+  {
+    quote:
+      "Collaborating with Offmarket was an absolute pleasure.their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. offmarket's enthusiasm for every facet of Marketing truly stands out. If you're seeking to elevate your  elevate your brand, offmarket is the ideal partner.",
+    name: "Muhsin Fazfary",
+    title: "Director of CM Academy",
+  },
+];
+
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Marketing strategist",
+    desc: "Developed and executed strategies to enhance brand recognition and market presence..",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Product Advertisement",
+    desc: "Designed and implemented marketing strategies to drive business growth and engagement...",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Branding",
+    desc: "Created and managed promotional campaigns to enhance product visibility and sales..",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Interactive Landing Page Designer",
+    desc: "Crafted visually appealing landing pages to improve user engagement and conversions..",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+

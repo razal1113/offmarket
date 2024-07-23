8a8d12248ec4bf0d83436f56fcaa71a932234e5e
razal <razalsha091@.com>
2024-07-24 04:03:34 +0530

    first commit

--- data/index.ts
+++ data/index.ts
@@ -28,7 +28,7 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "Driving Digital Success with Innovative Marketing..",
+    title: "GROW WITH US",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "GROW WITH US",
+    title: " Cutting-edge digital marketing for brand growth.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Cutting-edge digital marketing for brand growth.",
+    title: "Driving Digital Success with Innovative Marketing..",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
